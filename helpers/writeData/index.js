3d401ef97b98e961a08c6ace49b1f1b9b315868a
putrasatr <satriaputrapratama2002@gmail.com>
2021-07-15 08:48:10 +0700

    Bug Fixing Async Storage

--- helpers/writeData/index.js
+++ helpers/writeData/index.js
@@ -1,5 +1,4 @@
 const fs = require("fs")
-const { PATH_LOCAL_STORAGE_DB } = require("../../constants")
 
 const writeData = (data, path) => {
     fs.writeFileSync(__dirname + path, JSON.stringify(data, null, 3))
